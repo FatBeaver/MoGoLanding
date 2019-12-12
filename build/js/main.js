90e754d9eff12d9c00d513e1dd8987699df1a4b8
alexandr <childrenofbodom737@gmail.com>
2019-12-13 01:04:30 +0300

    What We Do Section && Accordeon

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
 
+    let chevron = document.querySelectorAll('.works > .works__item > .works__header > i');
+
+    for(let key in chevron) {
+        chevron[key].onclick = function(event) {
+            if (event.target.classList.contains('chevron-rotate') === true) {
+                return false;
+            }
+            
+            // === Некий незнакомец, если ты видишь этот ужас, то знай что я пытался через циклы
+            // === увы не получилось, js очень странный (или я слишком тупой) -_-. 
+            chevron[0].parentElement.nextElementSibling.classList.add('work-txt-toggle');
+            chevron[0].classList.remove('chevron-rotate');
+            chevron[1].parentElement.nextElementSibling.classList.add('work-txt-toggle');
+            chevron[1].classList.remove('chevron-rotate');
+            chevron[2].parentElement.nextElementSibling.classList.add('work-txt-toggle');
+            chevron[2].classList.remove('chevron-rotate');
+        
+            event.target.classList.toggle('chevron-rotate');
+            event.target.parentElement.nextElementSibling.classList.toggle('work-txt-toggle');
+        }
+    }
+});
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let chevron = document.querySelectorAll('.works > works__item > works__header > i');
+    console.log(chevron);
+    chevron.oncklick = function(event) {
+
+    }
+});
