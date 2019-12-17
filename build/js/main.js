9951a6d9eb5970447fa20674d4cc7fa93f35f34d
alexandr <childrenofbodom737@gmail.com>
2019-12-17 18:39:03 +0300

    Ready Landing

--- build/js/main.js
+++ build/js/main.js
@@ -75,13 +75,73 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let leftTestimonials = document.querySelector('.testimonials-left-chevron');
         leftTestimonials.onclick = function(event) {
             let direction = 'left';
-            slideMove(direction, '.testimonials-slider', '.testimonials-slider__wrapper', '.testimonials-slider__item');
+            slideMove(direction, '.testimonials-slider', '.testimonials-slider__wrapper', 
+            '.testimonials-slider__item');
         }
 
     let rightTestimonials = document.querySelector('.testimonials-right-chevron');
         rightTestimonials.onclick = function(event) {
             let direction = 'right';
-            slideMove(direction, '.testimonials-slider', '.testimonials-slider__wrapper', '.testimonials-slider__item');
+            slideMove(direction, '.testimonials-slider', '.testimonials-slider__wrapper', 
+            '.testimonials-slider__item');
         }
    
+    // =============== SECTION SCROLL ===============
+    let anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+        animationTime = 1000,
+        framesCount = 60;
+
+    anchors.forEach(function(item) {
+        item.addEventListener('click', function(e) {
+            e.preventDefault();
+         
+            let coordY = document.querySelector(item.getAttribute('href'))
+            .getBoundingClientRect().top + window.pageYOffset;
+            
+            let scroller = setInterval(function() {
+             
+                let scrollBy = coordY / framesCount;  
+                
+                if(scrollBy > window.pageYOffset - coordY && 
+                    window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+                    window.scrollBy(0, scrollBy);
+                } else {
+                    window.scrollTo(0, coordY);
+                    clearInterval(scroller);
+                }
+            }, animationTime / framesCount);
+        });
+    });
+
+    // ============ TOGGLE MENU ==============
+    let menuToggleButton = document.getElementsByClassName('menu__toggle');
+
+    menuToggleButton[0].addEventListener('click', function(event) {
+        if (menuToggleButton[0].classList.contains('close-menu')) {
+            hideMenuBar(event);
+        } else {
+            showMenuBar(event); 
+        }
+    });
+
+    function showMenuBar(event) {
+        menuToggleButton[0].innerHTML = '<i class="fas fa-times"></i>';
+        menuToggleButton[0].classList.add('close-menu');
+
+        let menuNav = document.querySelectorAll('.header-nav > .menu');
+        menuNav[0].style.display = 'flex';
+        menuNav[0].style.flexDirection = 'column';
+        menuNav[0].style.alignItems = 'flex-end';
+        menuNav[0].style.fontSize = '24px';
+        menuNav[0].style.marginTop = '30px';
+        menuNav[0].style.marginRight = '-17px';
+    }
+
+    function hideMenuBar(event) {
+        menuToggleButton[0].innerHTML = '<i class="fas fa-bars"></i>';
+        menuToggleButton[0].classList.remove('close-menu');
+
+        let menuNav = document.querySelectorAll('.header-nav > .menu');
+        menuNav[0].style.display = 'none';
+    }
 });
